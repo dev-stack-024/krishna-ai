ce6d44205792f7656e14609d499bdf97a7a61047
krishnagunda2997 <krishnagunda29@gmail.com>
2026-03-07 18:40:14 +0530

    cors

--- api/main.py
+++ api/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import requests
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 OLLAMA_URL = "http://ollama:11434/api/generate"
 EMBED_URL = "http://ollama:11434/api/embeddings"
